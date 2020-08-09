46f48272963c997f28fa8f7d9caa74c5ae356c4c
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-08-09 13:41:06 -0400

    routes for navbar links

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,25 @@ import React from "react";
 import Map from "./Containers/Map";
 import Navbar from "./Components/Navbar/Navbar";
 import './app.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Mappage from "./pages/Mappage";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <div>
+          <Navbar />
+          {/* <Route exact path="/" component={App} /> */}
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/map" component={Mappage} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/signup" component={Signup} />
+        </div>
+      </Router>
       <Map />
     </div>
   );
---
+++ client/src/pages/Mappage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Mappage() {
+  return (
+    <div>
+      <h1>Mappage Page</h1>
+
+    </div>
+  );
+}
+
+export default Mappage;
