88f02d3b9dce612f69f22e358648b872f35718f1
Diego Cordoba <diegocordoba87@hotmail.com>
2020-08-09 14:31:25 -0400

    added location controller

---
+++ apiRoutes/locationsController.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+
+router.get("/api/locations", (req, res) => {
+    db.Location.find({})
+      .then((foundLocations) => {
+        
+        res.json({
+          error: false,
+          data: foundLocations,
+          message: "All locations retrieved.",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: true,
+          data: null,
+          message: "Unable to retrieve all users.",
+        });
+      });
+  });
+
+  router.get("/api/locations/:id", (req, res) => {
+    db.Location.findById(req.params.id).populate({
+        path:"notes",
+        populate:{
+            model: "Note"
+        }
+    })
+      .then((foundLocations) => {
+        
+        res.json({
+          error: false,
+          data: foundLocations,
+          message: "All locations retrieved.",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: true,
+          data: null,
+          message: "Unable to retrieve all users.",
+        });
+      });
+  });
