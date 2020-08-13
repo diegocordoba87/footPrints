babfa018defe0e03830fe76f15aa6366ded32404
Diego Cordoba <diegocordoba87@hotmail.com>
2020-08-13 18:26:04 -0400

    geo location route added

--- API/routes/locationsRoutes.js
+++ API/routes/locationsRoutes.js
@@ -42,6 +42,39 @@ router.get("/api/locations", (req, res) => {
         });
       });
   })
+
+  router.get("/api/locationsnear", (req, res)=>{
+    let lng = parseFloat(req.query.lng)
+    let lat = parseFloat(req.query.lat)
+    console.log("lng", req.query.lng)
+    console.log("lat", req.query.lat)
+    db.Location.aggregate([
+      {
+        $geoNear: {
+           near: { type: "Point", coordinates: [ lng , lat ] },
+           distanceField: "dist.calculated",
+           maxDistance: 2,
+           spherical: true
+        }
+      }
+   ]).then((location)=>{
+      console.log(location)
+
+      res.json({
+        error: false,
+        data: location,
+        message: "All locations retrieved.",
+      })
+    }).catch((err)=>{
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to find location"
+      })
+    })
+  })
+
+
   
 router.post("/api/addlocation", (req, res)=>{
   db.Location.create(req.body)
