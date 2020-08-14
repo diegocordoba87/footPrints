a44227b765bc6edf71d7697015ca8a90831b03a4
Michael Nava <M1cha3l.nava@gmail.com>
2020-08-13 22:42:39 -0400

    modfird user routes

--- client/src/Components/Map.jsx
+++ client/src/Components/Map.jsx
@@ -141,16 +141,15 @@ export default class Map extends React.Component {
     map.addObject(morganFalls);
 
     let mfpoly = new H.map.Polygon({
-		//33.739023, -84.347441
-		lng: -84.347441 , lat: 33.739023,
-		//33.739041, 
-		lng: -84.346272 , lat: 33.739041,
+      //33.739023, -84.347441
+      lng: -84.347441,
+      lat: 33.739023,
+      //33.739041,
+      lng: -84.346272,
+      lat: 33.739041,
 
-		
-
-		lng: -84.347441 , lat: 33.739023
-
-
+      lng: -84.347441,
+      lat: 33.739023,
     });
 
     let elizabethPorterParkAndSprayground = new H.map.Circle(
